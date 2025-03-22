7273fa5eb506b5143557ed88a5cedad738017ca8
shubhamkrmahto <skm9258@gmail.com>
2025-03-22 17:01:50 +0530

    Added PATCH API for all fields

--- CLMS-Admin-API-Module/src/main/java/com/app/controller/EmployeeController.java
+++ CLMS-Admin-API-Module/src/main/java/com/app/controller/EmployeeController.java
@@ -3,12 +3,15 @@ package com.app.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.app.enums.EmployeeType;
 import com.app.service.EmployeeServiceI;
 
 @RestController
@@ -26,5 +29,47 @@ public class EmployeeController {
 		return new ResponseEntity<String>(msg,HttpStatus.CREATED);
 		
 	}
+	
+	@PatchMapping("/updatename/{id}/{name}")
+	public ResponseEntity<String> updateEmployeeName(@PathVariable("id") Integer id, @PathVariable("name") String name){
+		
+		String msg = employeeService.UpdateEmpName(id, name);
+		
+		return new ResponseEntity<String>(msg, HttpStatus.ACCEPTED);
+	}
+	
+	@PatchMapping("/updatemail/{id}/{email}")
+	public ResponseEntity<String> updateEmployeeEmail(@PathVariable("id") Integer id, @PathVariable("email") String email){
+		
+		String msg = employeeService.UpdateEmpEmail(id, email);
+		
+		return new ResponseEntity<String>(msg, HttpStatus.ACCEPTED);
+	}
+	
+	@PatchMapping("/updateempType/{id}/{type}")
+	public ResponseEntity<String> updateEmployeeType(@PathVariable("id") Integer id, @PathVariable("type") EmployeeType type){
+		
+		String msg = employeeService.UpdateEmpType(id, type);
+		
+		return new ResponseEntity<String>(msg, HttpStatus.ACCEPTED);
+	}
+	
+	@PatchMapping("/updatepassword/{id}/{pass}")
+	public ResponseEntity<String> updateEmployeePass(@PathVariable("id") Integer id, @PathVariable("pass") String pass){
+		
+		String msg = employeeService.UpdateEmpPass(id, pass);
+		
+		return new ResponseEntity<String>(msg, HttpStatus.ACCEPTED);
+	}
+	
+	@PatchMapping("/updatephoto/{id}")
+	public ResponseEntity<String> updateEmployeePhoto(@PathVariable("id") Integer id, @RequestPart("photo") MultipartFile photo){
+		
+		String msg = employeeService.UpdateEmpPhoto(id, photo);
+		
+		return new ResponseEntity<String>(msg, HttpStatus.ACCEPTED);
+	}
+	
+	
 
 }
--- CLMS-Admin-API-Module/src/main/java/com/app/service/EmployeeServiceI.java
+++ CLMS-Admin-API-Module/src/main/java/com/app/service/EmployeeServiceI.java
@@ -2,8 +2,19 @@ package com.app.service;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import com.app.enums.EmployeeType;
+
 public interface EmployeeServiceI {
 
 	public String saveEmployee(String emp, MultipartFile photo);
 
+	public String UpdateEmpName(Integer id, String name);
+	
+	public String UpdateEmpEmail(Integer id, String email);
+	
+	public String UpdateEmpType(Integer id, EmployeeType empType);
+	
+	public String UpdateEmpPass(Integer id, String pass);
+	
+	public String UpdateEmpPhoto(Integer id, MultipartFile photo);
 }
